77284d8b64c0997806b26ff6261351ac6fa3b467
Kaanan Kharwa <kaanan99@gmail.com>
2021-01-15 11:43:30 -0800

    Linked with back end and put works

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 import Table from './Table'
 import Form from './Form';
 class App extends Component {
@@ -22,6 +23,16 @@ class App extends Component {
       },
     ]
    }
+   componentDidMount(){
+    axios.get('http://localhost:5000/users')
+    .then(res => {
+      const characters = res.data.users_list;
+      this.setState({ characters });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+   }
     render() {
        const { characters } = this.state; 
       return (
@@ -41,9 +52,31 @@ class App extends Component {
    })
   }
   handleSubmit = character => {
-   this.setState({characters: [...this.state.characters, character]})
-  };
+   this.makePostCall(character).then( callResult => {
+      if (callResult === true) {
+         this.setState({ characters: [...this.state.characters, character] });
+      }
+   });
+ }
+makePostCall(character){
+   return axios.post('http://localhost:5000/users', character)
+    .then(function (response) {
+      console.log(response);
+      return (response.status === 200);
+    })
+    .catch(function (error) {
+      console.log(error);
+      return false;
+    });
+ }
+
+
 
 }
+
+
+
+
+
 
 export default App
